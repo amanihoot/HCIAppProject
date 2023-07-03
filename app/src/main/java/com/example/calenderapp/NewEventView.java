76a151ac8bfe0e1bcb169e1c73a620bda7ed55c4
Amani Harrison-Bryan <amanihoot@yahoo.com>
2023-07-03 14:58:35 -0500

    NewActivityandNewJavaAdded

---
+++ app/src/main/java/com/example/calenderapp/NewEventView.java
@@ -0,0 +1,17 @@
+package com.example.calenderapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewEventView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_new_event);
+
+
+    }
+}
