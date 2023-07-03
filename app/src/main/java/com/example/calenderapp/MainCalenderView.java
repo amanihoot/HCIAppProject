d69d8f40d89b4ddbc53167508a01259042db2c0f
Amani Harrison-Bryan <amanihoot@yahoo.com>
2023-07-02 21:18:22 -0500

    Loginpageadded SecondActivityAdded

---
+++ app/src/main/java/com/example/calenderapp/MainCalenderView.java
@@ -0,0 +1,15 @@
+package com.example.calenderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.constraintlayout.widget.ConstraintLayout;
+//import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainCalenderView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calender_main_view);
+    }
+}
