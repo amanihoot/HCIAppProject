a1e0c27936ba3db9e4b88dc5c6c56c8ccf2c3f57
Amani Harrison-Bryan <amanihoot@yahoo.com>
2023-07-04 20:48:37 -0500

    NewActivityandNewJavaAdded

--- app/src/main/java/com/example/calenderapp/MainCalenderView.java
+++ app/src/main/java/com/example/calenderapp/MainCalenderView.java
@@ -1,56 +1,60 @@
 package com.example.calenderapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-//import androidx.constraintlayout.widget.ConstraintLayout;
-//import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.CalendarView;
 
-public class MainCalenderView extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class NewEventView  extends AppCompatActivity {
 
-
-    private ArrayList<String> items;
-    private ArrayAdapter<String> itemsAdapter;
-    private addlist listview;
-    private Button button;
+    // Define the variable of CalendarView type
+    // and TextView type;
+    CalendarView calendar;
+    //TextView date_view;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calender_main_view);
+        setContentView(R.layout.add_new_event);
 
-        addList = findViewById(R.id.addList);
-        button = findViewById(R.id.button);
+        // By ID we can use each component
+        // which id is assign in xml file
+        // use findViewById() to get the
+        // CalendarView and TextView
+        calendar = (CalendarView)
+                findViewById(R.id.calendar);
+      /*  date_view = (TextView)
+                findViewById(R.id.date_view);*/
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addList(view);
-            }
-        });
+        // Add Listener in calendar
+        calendar
+                .setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
 
-        items = new ArrayList<>();
-        itemsAdapter = new ArrayAdapter<>( context: this, android.R.layout.simple_list_item_1, items)
-        listView.setAdapter(itemsAdapter);
-        setUpListViewListener();
-        }
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth)
+                            {
 
-        private voide addList(View view) {
-        listView.SetOnItemClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long 1) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, text: "Item Removed", Toast.LENGTH_LONG).show();
+                                // Store the value of date with
+                                // format in String type Variable
+                                // Add 1 in month because month
+                                // index is start with 0
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
 
-                items.remove(i);
-                itemsAdapter.notifyDataSetChanged();
-
-            }
-        });
-
-        }
-
+                                // set this date in TextView for Display
+                                /* date_view.setText(Date);*/
+                            }
+                        });
+    }
 }
